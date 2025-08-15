02458d7cdd6350adebb477e98753270948feb9d8
miggy88 <cunananluismiguel11@gmail.com>
2025-08-15 16:40:09 +0800

    step 7

--- MiguelProject/Form1.cs
+++ MiguelProject/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MiguelProject;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -24,6 +25,8 @@ namespace MiguelProject
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+            //LastName
+            StudentInfoClass.StudentNumber = Convert.ToInt64(studentNumbertxtbox.Text);
 
         }
 
@@ -36,5 +39,75 @@ namespace MiguelProject
         {
 
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Button 
+
+
+           
+        }
+
+        private void lastNametxtbox_TextChanged(object sender, EventArgs e)
+        {
+            //lastname
+
+            StudentInfoClass.LastName = lastNametxtbox.Text;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            //FirstName
+
+            StudentInfoClass.FirstName = firstNametxtbox.Text;
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            //MiddleName
+            StudentInfoClass.MiddleName = middleNametxtbox.Text;
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            //Program   
+            StudentInfoClass.Program = programtxtbox.Text;
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            //Age 
+
+            StudentInfoClass.Age = Convert.ToInt64(agetxtbox.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            //Address
+
+            StudentInfoClass.Address = addresstxtbox.Text;
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            //ContactNumber
+            StudentInfoClass.ContactNumber = Convert.ToInt64(contactNotxtbox.Text);
+
+
+
+
+        }
     }
 }
+
