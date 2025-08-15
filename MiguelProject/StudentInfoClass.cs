5023fb93ff3d0b4ff4dbd0089f4cc0da0e49f831
miggy88 <cunananluismiguel11@gmail.com>
2025-08-15 15:48:00 +0800

    step 4

--- MiguelProject/StudentInfoClass.cs
+++ MiguelProject/StudentInfoClass.cs
@@ -12,5 +12,15 @@ namespace MiguelProject
         public delegate void DelegateText(String txt);
 
 
+        public static string FirstName = "";
+        public static string LastName = "";
+        public static string MiddleName = "";
+        public static string Address = "";
+        public static string Program = "";
+        public static long Age = 0;
+        public static long ContactNumber = 0;
+        public static long  StudentNumber = 0;
+
+
     }
 }
