2c961555be18ac8f4c17c188b2db13b663350886
miggy88 <cunananluismiguel11@gmail.com>
2025-08-15 16:00:12 +0800

    Step 5

--- MiguelProject/StudentInfoClass.cs
+++ MiguelProject/StudentInfoClass.cs
@@ -21,6 +21,46 @@ namespace MiguelProject
         public static long ContactNumber = 0;
         public static long  StudentNumber = 0;
 
+        public static string GetFirstName(string FirstName)
+        {
+            return FirstName;
+        }
+        public static string GetLastName(string LastName)
+        {
+            return LastName;
+        }
+
+        public static string GetMiddleName(string MiddleName)
+        {
+            return MiddleName;
+        }
+
+        public static string GetAddress(string Address)
+        {
+            return Address;
+        }
+
+        public static string GetProgram(string Program)
+        {
+            return Program;
+        }
+
+        public static long GetAge(long Age)
+        {
+            return Age;
+        }
+
+        public static long GetContactNumber(long ContactNumber)
+        {
+            return ContactNumber;
+        }   
+
+        public static long GetStudentNumber(long StudentNumber)
+        {
+            return StudentNumber;
+        }
+
+
 
     }
 }
