6cd3c7ce94c9958c2b845e4bb02f6ffb96e023d7
miggy88 <cunananluismiguel11@gmail.com>
2025-08-15 15:36:45 +0800

    Step 3.

---
+++ MiguelProject/StudentInfoClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiguelProject
+{
+    class StudentInfoClass
+    {
+        public delegate int DelegateNumber(long number);
+        public delegate void DelegateText(String txt);
+
+
+    }
+}
